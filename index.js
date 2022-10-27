a622b3909aee3e36716ee4b33425f119de1d756a
Xanul <rivas.060@gmail.com>
2022-10-27 18:41:24 -0500

    Added dotenv

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // Importando el paquete de express
 const express = require('express');
+require('dotenv').config();
 
 // Creando el servidor de express
 const app = express();
 
+// Directorio publico
+app.use( express.static('public') );
+
 // Rutas
-app.get('/', (req, res) => {
-  res.json({
-    ok: true,
-    hola: "mundo"
-  })
-})
+// app.get('/', (req, res) => {
+//   res.json({
+//     ok: true,
+//     hola: "mundo"
+//   })
+// })
+
+
 
 
 // Escuchar peticiones
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${4000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
