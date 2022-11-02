c9fac3615f24ec3864c73611a00036256cdc3648
Xanul <rivas.060@gmail.com>
2022-11-01 18:11:38 -0600

    Added user login validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,24 +37,53 @@ const createUser = async (req, res = response) => {
     res.status(500).json({
       ok: false,
       msg: 'Please call admin'
-    })
-    
+    }) 
   }
-
-  
 }
 
-const userLogin = (req, res = response) => {
+const userLogin = async (req, res = response) => {
   
   const { email, password } = req.body
 
-  res.status(200).json({
-    ok: true,
-    msg: 'User Login',
-    email,
-    password
-  })
+  try {
+    
+    const user = await User.findOne({ email: email });
+
+    if ( !user ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'There is no user registered with that email'
+      })
+    }
+
+    // Confirmar contraseñas
+    const validPassword = bcrypt.compareSync( password, user.password );
+    if ( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Incorrect password'
+      })
+    }
+
+    // Generar nuestro JWT
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please call admin'
+    }) 
+  }
+
+
 }
+
+
 
 const tokenRenew = (req, res = response) => {
   res.json({
